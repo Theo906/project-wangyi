d2813d6e70520bd82421184f9a6bf3167a162b08
Theo <515940017@qq.com>
2020-08-22 21:31:31 +0800

    hh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="我是Theo vue-cli 4.0" />
   </div>
 </template>
 
